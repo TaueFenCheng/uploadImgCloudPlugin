315ac49a04196ec530eaf05e3648409fb70fc0c6
tangjiaqiang <tangjiaqiang@heytea.com>
2025-04-01 11:59:28 +0800

    feat: plugin update init

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import type { RsbuildPlugin } from '@rsbuild/core';
 
-export type PluginExampleOptions = {
+export type uploadImgCloudPluginType = {
   foo?: string;
   bar?: boolean;
 };
 
-export const pluginExample = (
-  options: PluginExampleOptions = {},
+export const uploadImgCloudPlugin = (
+  options: uploadImgCloudPluginType = {},
 ): RsbuildPlugin => ({
-  name: 'plugin-example',
-
-  setup() {
+  name: 'uploadImgCloudPlugin',
+  setup(api) {
+    const config = api.getRsbuildConfig();
+    console.log(config.html?.title);
     console.log('Hello Rsbuild!', options);
   },
 });
